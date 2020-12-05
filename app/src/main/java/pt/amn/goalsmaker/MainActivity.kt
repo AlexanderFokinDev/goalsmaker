dcf8fafc0899c579f502480fc99b171050bc36b4
Alexander Fokin <alexanderfokindev@gmail.com>
2020-12-05 20:56:12 +0300

    Upgrade the main activity

--- app/src/main/java/pt/amn/goalsmaker/MainActivity.kt
+++ app/src/main/java/pt/amn/goalsmaker/MainActivity.kt
@@ -5,127 +5,65 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import androidx.appcompat.widget.Toolbar
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import pt.amn.goalsmaker.adapters.SQLBigGoalsAdapter
+import pt.amn.goalsmaker.databinding.ActivityMainBinding
 import pt.amn.goalsmaker.models.BigGoalModel
 
-class MainActivity : AppCompatActivity(), View.OnClickListener
-    , SQLBigGoalsAdapter.SQLBigGoalsAdapterCallback {
+class MainActivity : AppCompatActivity(), SQLBigGoalsAdapter.SQLBigGoalsAdapterCallback {
 
-    //private val rvBigGoals : RecyclerView  = findViewById(R.id.rv_big_goals);
-
-    private val dbHelper = DBHelper(this)
-    private var mBigGoalsList = listOf<BigGoalModel>()
-    private var adapter =
-        SQLBigGoalsAdapter(mBigGoalsList, this, this)
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var adapter : SQLBigGoalsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         initializationView()
-        initializationRecyclerView()
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        refreshRecyclerView()
-
     }
 
     private fun initializationView() {
 
-        // Action bar
-        val toolbar : Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        val actionBar = supportActionBar
-        //actionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.run {
+            setSupportActionBar(includedToolbar.toolbar)
 
-        val mFabButton : FloatingActionButton = findViewById(R.id.fab_big_goals)
-        mFabButton.setOnClickListener(this)
-    }
+            adapter = SQLBigGoalsAdapter(this@MainActivity, this@MainActivity)
+            rvBigGoals.setHasFixedSize(true)
+            rvBigGoals.adapter = adapter
 
-    private fun initializationRecyclerView() {
-
-        // TODO
-        //val testString = "Daily plan\t\t\t\t\t73%\nWeekly plan\t\t\t\t20%\nMontly plan\t\t\t\t84%"
-
-        /*val testBigGoal = BigGoalModel()
-        testBigGoal.title = "Test"
-        testBigGoal.description = testString
-
-        val testBigGoal1 = BigGoalModel()
-        testBigGoal1.title = "Test1"
-        testBigGoal1.description = testString
-
-        mBigGoalsList.add(testBigGoal)
-        mBigGoalsList.add(testBigGoal1)*/
-
-        //dbHelper.addBigGoal(testBigGoal)
-        //dbHelper.addBigGoal(testBigGoal1)
-        //val mBigGoalsList = dbHelper.getAllBigGoals()
-
-        //////////////////////
-
-        val rvBigGoals : RecyclerView  = findViewById(R.id.rv_big_goals)
-
-        rvBigGoals.setHasFixedSize(true)
-        rvBigGoals.layoutManager = LinearLayoutManager(this
-                                , LinearLayoutManager.VERTICAL, false)
-        rvBigGoals.adapter = adapter
-
+            fabBigGoals.setOnClickListener {
+                startActivity(Intent(this@MainActivity, BigGoalActivity::class.java))
+            }
+        }
+        updateData()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
         menuInflater.inflate(R.menu.main_menu, menu)
-
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        val id = item.itemId
-
-        if (id == R.id.mainmenu_action_exit) {
-            finish()
-        }
-        else if (id == R.id.mainmenu_action_settings) {
-            val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
-            //finish()
+        when (item.itemId) {
+            R.id.mainmenu_action_exit -> finish()
+            R.id.mainmenu_action_settings ->
+                startActivity(Intent(this, SettingsActivity::class.java))
         }
 
         return super.onOptionsItemSelected(item)
     }
 
-    override fun onClick(v: View?) {
-
-        if (v?.id == R.id.fab_big_goals) {
-            val intent = Intent(this, BigGoalActivity::class.java)
-            startActivity(intent)
-        }
-
-    }
-
-    fun refreshRecyclerView() {
-        mBigGoalsList = dbHelper.getAllBigGoals()
-        adapter.setItems(mBigGoalsList)
+    private fun updateData() {
+        adapter.setItems()
         adapter.notifyDataSetChanged()
     }
 
-    override fun onBigGoalClick(pos: Int) {
-        var goal = mBigGoalsList.get(pos)
-
+    override fun onBigGoalClick(bigGoal: BigGoalModel) {
         val intent = Intent(this, BigGoalActivity::class.java)
-        intent.putExtra(BigGoalActivity.EXTRA_PARAM_GOAL, goal)
+        intent.putExtra(BigGoalActivity.EXTRA_PARAM_GOAL, bigGoal)
         startActivity(intent)
-        //finish()
     }
 
 }
--- app/src/main/java/pt/amn/goalsmaker/adapters/SQLBigGoalsAdapter.kt
+++ app/src/main/java/pt/amn/goalsmaker/adapters/SQLBigGoalsAdapter.kt
@@ -2,89 +2,73 @@ package pt.amn.goalsmaker.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import pt.amn.goalsmaker.DBHelper
 import pt.amn.goalsmaker.models.BigGoalModel
-import pt.amn.goalsmaker.R
 import pt.amn.goalsmaker.Utils
+import pt.amn.goalsmaker.databinding.RowBigGoalBinding
 
-class SQLBigGoalsAdapter (private var mAllBigGoals : List<BigGoalModel>
-                          , val listener : SQLBigGoalsAdapterCallback,
-                          val context: Context)
+class SQLBigGoalsAdapter (private val listener : SQLBigGoalsAdapterCallback, val context: Context)
     : RecyclerView.Adapter<SQLBigGoalsAdapter.BigGoalsViewHolder> ()
      {
+         private var mAllBigGoals : List<BigGoalModel> = listOf()
 
-    interface SQLBigGoalsAdapterCallback {
-        fun onBigGoalClick(pos : Int)
-    }
+        interface SQLBigGoalsAdapterCallback {
+            fun onBigGoalClick(bigGoal: BigGoalModel)
+        }
 
-    //private var listener : SQLBigGoalsAdapterCallback
-    //private var mAllBigGoals : List<BigGoalModel> = list
-    //private val listener  = listener
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        class BigGoalsViewHolder(private val binding : RowBigGoalBinding
+            , private val listener: SQLBigGoalsAdapterCallback, val context: Context)
+            : RecyclerView.ViewHolder(binding.root) {
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
-    class BigGoalsViewHolder(itemView: View, val listener : SQLBigGoalsAdapterCallback)
-        : RecyclerView.ViewHolder(itemView) {
+                fun onBind(bigGoal : BigGoalModel) {
 
-        val tvTitle : TextView = itemView.findViewById(R.id.row_big_goal_tv_title)
-        val tvDescription : TextView = itemView.findViewById(R.id.row_big_goal_tv_description)
-        //cbVisitedCountry = (CheckBox) itemView.findViewById(R.id.country_row_check);
-        val ivGoal : ImageView = itemView.findViewById(R.id.row_big_goal_iv)
+                    binding.run {
+                        tvTitle.text = bigGoal.title
+                        tvDescription.text = bigGoal.description
 
-    }
+                        ivBigGoal.setOnClickListener {
+                            listener.onBigGoalClick(bigGoal)
+                        }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
-            : BigGoalsViewHolder {
+                        val imageFile = Utils(context.applicationContext)
+                            .getImagePathFromInternalStorage(bigGoal.imagePath)
+                        if (imageFile.exists()) {
+                            Glide.with(context)
+                                .load(imageFile)
+                                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                                .skipMemoryCache(true)
+                                .into(ivBigGoal)
+                        }
+                    }
 
-        val v : View = LayoutInflater.from(parent.context)
-            .inflate(R.layout.row_big_goal, parent, false)
+                }
 
-        return BigGoalsViewHolder(
-            v,
-            listener
-        )
+        }
 
-    }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+                : BigGoalsViewHolder {
+            return BigGoalsViewHolder(RowBigGoalBinding
+                .inflate(LayoutInflater.from(parent.context), parent, false), listener
+                , context)
+        }
 
-    override fun getItemCount(): Int {
-        return mAllBigGoals.count()
-    }
+        override fun getItemCount(): Int {
+            return mAllBigGoals.size
+        }
 
-    override fun onBindViewHolder(holder: BigGoalsViewHolder, position: Int) {
+        override fun onBindViewHolder(holder: BigGoalsViewHolder, position: Int) {
+            holder.onBind(mAllBigGoals[position])
+        }
 
-        val bigGoal = mAllBigGoals.get(position)
-
-        holder.tvTitle.text = bigGoal.title
-        holder.tvDescription.text = bigGoal.description
-
-        holder.ivGoal.setOnClickListener(View.OnClickListener {
-            listener.onBigGoalClick(position)
-        })
-
-        val imageFile = Utils(context.applicationContext)
-            .getImagePathFromInternalStorage(bigGoal.imagePath)
-        if (imageFile.exists())
-            Glide.with(context)
-                .load(imageFile)
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .skipMemoryCache(true)
-                .into(holder.ivGoal)
-
-        /*countriesViewHolder.tvCountry.setText(country.getLocalCountryName());
-        countriesViewHolder.cbVisitedCountry.setChecked(country.visited);
-        countriesViewHolder.ivFlag.setImageResource(country.flagResId);*/
-
-    }
-
-     fun setItems(list : List<BigGoalModel>) {
-         mAllBigGoals = list
-     }
+         fun setItems() {
+             mAllBigGoals = DBHelper(context).getAllBigGoals()
+         }
 
 }
